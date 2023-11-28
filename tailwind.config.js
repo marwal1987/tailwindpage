b6fc1399480c77a62316394f3aeba0401e02311e
marwal1987 <marwal1987@gmail.com>
2023-11-28 17:47:25 +0100

    removed code and added original copy index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
+// På något sätt ska man lyckas få in google fonts
 // const defaultTheme = require('tailwindcss/defaultTheme');
 
 // module.exports = {
