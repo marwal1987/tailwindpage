1712d340ed06740c96857a2687c6b09cdfe4d071
marwal1987 <marwal1987@gmail.com>
2023-11-30 13:04:47 +0100

    fixed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Inter"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
-
-// På något sätt ska man lyckas få in google fonts
-// const defaultTheme = require('tailwindcss/defaultTheme');
-
-// module.exports = {
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         inter: ['"Inter"', ...defaultTheme.fontFamily.sans]
-//       }
-//     }
-//   },
-// }
